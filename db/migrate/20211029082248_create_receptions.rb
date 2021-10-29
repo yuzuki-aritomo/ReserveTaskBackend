778fd52645cdd68c21e061c5aaf2754bb34c2c68
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-10-29 17:26:20 +0900

    add: reception migration file

---
+++ db/migrate/20211029082248_create_receptions.rb
@@ -0,0 +1,9 @@
+class CreateReceptions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :receptions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.datetime :date, null: false, index: true
+      t.timestamps
+    end
+  end
+end
