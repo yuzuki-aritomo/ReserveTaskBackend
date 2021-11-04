dbf16e208cd85e5d97de46069e9350adbe45a79a
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-11-04 16:23:14 +0900

    fix: unsignedの追加

--- db/migrate/20211029082248_create_receptions.rb
+++ db/migrate/20211029082248_create_receptions.rb
@@ -1,6 +1,6 @@
 class CreateReceptions < ActiveRecord::Migration[6.0]
   def change
-    create_table :receptions do |t|
+    create_table :receptions, unsigned: true do |t|
       t.references :user, null: false, foreign_key: true
       t.datetime :date, null: false, index: true
       t.timestamps
--- db/migrate/20211029082949_create_reservations.rb
+++ db/migrate/20211029082949_create_reservations.rb
@@ -1,6 +1,6 @@
 class CreateReservations < ActiveRecord::Migration[6.0]
   def change
-    create_table :reservations do |t|
+    create_table :reservations, unsigned: true do |t|
       t.references :user,         null: false,  foreign_key: true
       t.references :reception, null: false,  foreign_key: true
       t.boolean :cancel_flag,   null: false,  :default => false
