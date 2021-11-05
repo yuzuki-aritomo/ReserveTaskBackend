eedddc2b2b578be16699f835fd8d7c0673d42bd9
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-11-05 17:23:46 +0900

    fix: migrationファイルの変更

--- db/migrate/20211029082248_create_receptions.rb
+++ db/migrate/20211029082248_create_receptions.rb
@@ -1,10 +1,10 @@
 class CreateReceptions < ActiveRecord::Migration[6.0]
   def change
     create_table :receptions, unsigned: true do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, unsigned: true
       t.datetime :date, null: false, index: true
       t.timestamps
     end
-    add_index :receptions, [:user, :date], unique: true
+    add_index :receptions, [:user_id, :date], unique: true
   end
 end
--- db/migrate/20211029082949_create_reservations.rb
+++ db/migrate/20211029082949_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[6.0]
   def change
     create_table :reservations, unsigned: true do |t|
-      t.references :user,         null: false,  foreign_key: true
-      t.references :reception, null: false,  foreign_key: true
+      t.references :user,         null: false,  foreign_key: true, unsigned: true
+      t.references :reception, null: false,  foreign_key: true, unsigned: true
       t.boolean :cancel_flag,   null: false,  :default => false
       t.timestamps
     end
