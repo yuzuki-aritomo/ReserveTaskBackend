cd8d5574e3f23c22cf6b4034b7adde15663a30a8
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-11-18 16:46:25 +0900

    add: 日付処理

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -1,19 +1,21 @@
 class ReceptionsController < ApplicationController
+  # user 認証入れる
+  # before_action :authenticate_user!
   def create
     request_body = JSON.parse(request.body.read, {:symbolize_names => true})
     register_dates = request_body[:register_date]
     reception_dates = []
     register_dates.each do |register_date|
       @reception = Reception.new(
-        user_id: 1,
+        user_id: 1, # current_user.id
         date: register_date,
       )
       if @reception.save
         reception_dates.push({
           "reception_id": @reception.id,
-          "user_name": "taro",
-          "start": @reception.date,
-          "end": @reception.date + Rational(1, 24 * 60) ,
+          "user_name": "taro", # current_user.name
+          "start": @reception.date.iso8601,
+          "end": (@reception.date + 60*30).iso8601,
           "reserved": false,
           "canceled": false
         })
