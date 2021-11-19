1397129a90c4c0fb56031e2adb1dfccfd39540b1
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-11-19 13:39:59 +0900

    add: validation check

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -1,19 +1,19 @@
 class ReceptionsController < ApplicationController
-  # user 認証入れる
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
+  
   def create
     request_body = JSON.parse(request.body.read, {:symbolize_names => true})
     register_dates = request_body[:register_date]
     reception_dates = []
     register_dates.each do |register_date|
       @reception = Reception.new(
-        user_id: 1, # current_user.id
+        user_id: current_user.id,
         date: register_date,
       )
       if @reception.save
         reception_dates.push({
           "reception_id": @reception.id,
-          "user_name": "taro", # current_user.name
+          "user_name": current_user.name,
           "start": @reception.date.iso8601,
           "end": (@reception.date + 60*30).iso8601,
           "reserved": false,
@@ -21,7 +21,6 @@ class ReceptionsController < ApplicationController
         })
       end
     end
-
     response = {
       "data": reception_dates
     }
--- app/models/reception.rb
+++ app/models/reception.rb
@@ -3,17 +3,17 @@ class Reception < ApplicationRecord
   validates :user_id, presence: true
   validates :date, presence: true
 
+  validate :user_check_role
   validate :date_check_in_the_past
   validate :date_check_in_sunday
-  validate :date_check_in_reception_time
   validate :date_check_in_reception_hour
-  validate :user_check_role
+  validate :date_check_in_reception_min_sec
 
   #fp userかチェック
   def user_check_role
     @user = User.find(user_id)
     if !@user.is_FP
-      errors.add(:date, ": FP以外登録できません")
+      errors.add(:user, ": Financial Planner以外登録できません")
     end
   end
   #過去の日付は弾く
@@ -29,7 +29,7 @@ class Reception < ApplicationRecord
     end
   end
   #予約可能時間のチェック
-  def date_check_in_reception_time
+  def date_check_in_reception_hour
     if date.present?
       if date.saturday? && (date.hour < 11 || 15 <= date.hour )
         errors.add(:date, ": 予約受付時間外は登録できません")
@@ -39,9 +39,11 @@ class Reception < ApplicationRecord
     end
   end
   #30分単位のみ受付る
-  def date_check_in_reception_hour
-    if date.present? && (date.min != 00 || date.min != 30 || date.sec != 00)
-      errors.add(:date, ": 予約開始時間がずれています")
+  def date_check_in_reception_min_sec
+    if date.present?
+      if date.sec != 00 || !(date.min == 00 || date.min == 30)
+        errors.add(:date, ": 予約開始時間がずれています")
+      end
     end
   end
 end
