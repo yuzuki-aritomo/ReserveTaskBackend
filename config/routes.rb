1aba2751d456eca8941d6a5e040893b01bd776d0
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-11-26 13:45:48 +0900

    add: receptions delete

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "receptions"      => "receptions#index"
   post "receptions"      => "receptions#create"
+  delete "receptions/:id" => "receptions#delete"
 
 end
