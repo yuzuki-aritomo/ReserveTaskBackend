86b0436a3e8cf7aaa16c0f035dd3a58c7cca884c
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-10 10:33:42 +0900

    enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,11 @@ class User < ActiveRecord::Base
   has_many :reception, dependent: :destroy
   has_many :reservation, dependent: :destroy
 
+  enum role: { customer: 1, fp: 2 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :validatable
   include DeviseTokenAuth::Concerns::User
-
-  def fp?
-    role == 2
-  end
-
-  def user?
-    role == 1
-  end
 end
