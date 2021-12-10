e1cede57ccb19e75557b42160e395fc3a6633bf7
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-10 15:40:57 +0900

    add form

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -2,30 +2,11 @@ class ReceptionsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    register_dates = params.require(:register_date)
-    reception_dates = []
-    error_dates = []
-    register_dates.each do |register_date|
-      @reception = current_user.reception.build(date: register_date)
-      if @reception.save
-        reception_dates.push({
-          "reception_id": @reception.id,
-          "user_name": current_user.name,
-          "start": @reception.date.iso8601,
-          "end": (@reception.date + 60 * 30).iso8601,
-          "reserved": false
-        })
-      else
-        error_dates.push({
-          "date": @reception.date,
-          "error_messages": @reception.errors.full_messages
-        })
-      end
-    end
-    response = {
-      "data": reception_dates,
-      "error": error_dates
-    }
+    reception_form = ReceptionsRegistrationForm.new(
+      current_user,
+      params.require(:register_date)
+    )
+    response = reception_form.execute
     render json: response
   end
 end
