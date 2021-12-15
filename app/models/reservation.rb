6c6af79c8317841e292e60f4a080391d38670a88
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-15 19:47:53 +0900

    merge

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,11 +1,4 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :reception
-<<<<<<< HEAD
-
-  def get_user_name
-    user.name
-  end
-=======
->>>>>>> feature/receptions_get
 end
