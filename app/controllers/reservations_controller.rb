14a799ebf3a5c60b51b225841e91d2b7b4c59442
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-16 11:48:21 +0900

    fix rubocop

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -21,24 +21,17 @@ class ReservationsController < ApplicationController
   end
 
   def index
-    def convert_to_date(date)
-      begin
-        return Time.parse(date)
-      rescue
-        return nil
-      end
-    end
-    start_date = convert_to_date(params[:start]) || Time.now.prev_month
-    end_date = convert_to_date(params[:end]) || Time.now.next_month
+    start_date = convert_to_date(params[:start]) || Time.zone.now.prev_month
+    end_date = convert_to_date(params[:end]) || Time.zone.now.next_month
     @reservations = current_user.reservation.joins(:reception)
-    .select('reservations.*, receptions.date')
-    .where(receptions: {date: start_date ... end_date})
+                                .select('reservations.*, receptions.date')
+                                .where(receptions: { date: start_date...end_date })
     reservation_dates = []
-    @reservations.map do | reservation|
+    @reservations.map do |reservation|
       reservation_dates.push({
         'reservation_id': reservation.id,
         'start': reservation.date.iso8601,
-        'end': (reservation.date+60*30).iso8601,
+        'end': (reservation.date + 60 * 30).iso8601,
         'canceled': reservation.cancel_flag
       })
     end
