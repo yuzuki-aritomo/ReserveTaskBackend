df1c4593c15745b809d074b4b0d1a39e7e328299
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-16 16:34:04 +0900

    validate

--- app/models/reception.rb
+++ app/models/reception.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Reception < ApplicationRecord
+  has_many :reservation, dependent: :destroy
   belongs_to :user
 
   validates :user_id, presence: true
@@ -18,24 +19,27 @@ class Reception < ApplicationRecord
   end
 
   def validate_received_at
+    unless received_at.present?
+      errors.add(:date, ': 日時が登録されていません')
+    end
     # 過去の日付は弾く
-    if received_at.present? && received_at < Date.today
+    if received_at < Date.today
       errors.add(:date, ': 過去の日時は登録できません')
     end
     # 日曜日は弾く
-    if received_at.present? && received_at.sunday?
+    if received_at.sunday?
       errors.add(:date, ': 日曜日は登録できません')
     end
     # 予約可能時間のチェック
     if received_at.present?
-      if received_at.saturday? && (received_at.hour < 11 || 15 <= received_at.hour)
+      if received_at.hour < 11 || 15 <= received_at.hour
         errors.add(:date, ': 予約受付時間外は登録できません')
       elsif received_at.hour < 10 || 18 <= received_at.hour
         errors.add(:date, ': 予約受付時間外は登録できません')
       end
     end
     # 30分単位のみ受付る
-    if received_at.present? && (received_at.sec != 0o0 || !(received_at.min == 0o0 || received_at.min == 30))
+    if received_at.sec != 0o0 || !(received_at.min == 0o0 || received_at.min == 30)
       errors.add(:date, ': 予約開始時間がずれています')
     end
   end
