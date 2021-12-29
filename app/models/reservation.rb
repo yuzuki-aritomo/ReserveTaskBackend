def01474e333d3c02ef7d0545b659a8f0aeb477f
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2021-12-29 15:33:53 +0900

    add validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,19 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :reception
+
+  validate :validate_customer_user
+  validate :validate_reserved
+
+  def validate_customer_user
+    unless user.customer?
+      errors.add(:user, ': Customer以外予約できません')
+    end
+  end
+
+  def validate_reserved
+    if reception.reserved?
+      errors.add(:reservation, ': 予約済みのため予約できません')
+    end
+  end
 end
